f19f6bce2509bea052678473ba541de7481edeb6
lojason71 <lojason71@gmail.com>
2023-04-22 19:01:02 -0700

    Fixed bug for O to never win

--- lab2-files/main.js
+++ lab2-files/main.js
@@ -152,12 +152,25 @@ function removeListeners () {
 }
 
 function checkWin() {
-    if(turn % 5 != 0 && turn != 0) {
-        for (let i = 0; i < 3; i++) {
-            if (arr[i][0] !== "" && arr[i][0] === arr[i][1] && arr[i][1] === arr[i][2]) {
-                winner.innerHTML = "Player " + arr[i][0] +  " is the winner";
+    for (let i = 0; i < 3; i++) {
+        if (arr[i][0] !== "" && arr[i][0] === arr[i][1] && arr[i][1] === arr[i][2]) {
+            winner.innerHTML = "Player " + arr[i][0] +  " is the winner";
+            displayturn.innerHTML = "";
+            if(arr[i][0] == 'X') { player1 += 1; player1score.innerHTML = player1; }
+            else { player2 += 1; player2score.innerHTML = player2; }
+            removeListeners();
+            clearInterval(countdown);
+            // clearInterval(timerPlayer);
+            clearTimeout(time);
+            document.getElementById("playertimer").innerHTML = "";
+            document.getElementById("timer").innerHTML = "";
+            return;
+        }
+        for (let j = 0; j < 3; j++) {
+            if (arr[0][j] !== "" && arr[0][j] === arr[1][j] && arr[1][j] === arr[2][j]) {
+                winner.innerHTML = "Player " + arr[0][j] +  " is the winner";
                 displayturn.innerHTML = "";
-                if(arr[i][0] == 'X') { player1 += 1; player1score.innerHTML = player1; }
+                if(arr[0][j] == 'X') { player1 += 1; player1score.innerHTML = player1; }
                 else { player2 += 1; player2score.innerHTML = player2; }
                 removeListeners();
                 clearInterval(countdown);
@@ -167,47 +180,32 @@ function checkWin() {
                 document.getElementById("timer").innerHTML = "";
                 return;
             }
-            for (let j = 0; j < 3; j++) {
-                if (arr[0][j] !== "" && arr[0][j] === arr[1][j] && arr[1][j] === arr[2][j]) {
-                    winner.innerHTML = "Player " + arr[0][j] +  " is the winner";
-                    displayturn.innerHTML = "";
-                    if(arr[0][j] == 'X') { player1 += 1; player1score.innerHTML = player1; }
-                    else { player2 += 1; player2score.innerHTML = player2; }
-                    removeListeners();
-                    clearInterval(countdown);
-                    // clearInterval(timerPlayer);
-                    clearTimeout(time);
-                    document.getElementById("playertimer").innerHTML = "";
-                    document.getElementById("timer").innerHTML = "";
-                    return;
-                }
-            }
-            if (arr[0][0] !== "" && arr[0][0] === arr[1][1] && arr[1][1] === arr[2][2]) {
-                winner.innerHTML = "Player " + arr[0][0] +  " is the winner";
-                displayturn.innerHTML = "";
-                if(arr[0][0] == 'X') { player1 += 1; player1score.innerHTML = player1; }
-                else { player2 += 1; player2score.innerHTML = player2; }
-                removeListeners();
-                clearInterval(countdown);
-                // clearInterval(timerPlayer);
-                clearTimeout(time);
-                document.getElementById("playertimer").innerHTML = "";
-                document.getElementById("timer").innerHTML = "";
-                return;
-            }
-            if (arr[0][2] !== "" && arr[0][2] === arr[1][1] && arr[1][1] === arr[2][0]) {
-                winner.innerHTML = "Player " + arr[0][2] +  " is the winner";
-                displayturn.innerHTML = "";
-                if(arr[0][2] == 'X') { player1 += 1; player1score.innerHTML = player1; }
-                else { player2 += 1; player2score.innerHTML = player2; }
-                removeListeners();
-                clearInterval(countdown);
-                // clearInterval(timerPlayer);
-                clearTimeout(time);
-                document.getElementById("playertimer").innerHTML = "";
-                document.getElementById("timer").innerHTML = "";
-                return;
-            }
+        }
+        if (arr[0][0] !== "" && arr[0][0] === arr[1][1] && arr[1][1] === arr[2][2]) {
+            winner.innerHTML = "Player " + arr[0][0] +  " is the winner";
+            displayturn.innerHTML = "";
+            if(arr[0][0] == 'X') { player1 += 1; player1score.innerHTML = player1; }
+            else { player2 += 1; player2score.innerHTML = player2; }
+            removeListeners();
+            clearInterval(countdown);
+            // clearInterval(timerPlayer);
+            clearTimeout(time);
+            document.getElementById("playertimer").innerHTML = "";
+            document.getElementById("timer").innerHTML = "";
+            return;
+        }
+        if (arr[0][2] !== "" && arr[0][2] === arr[1][1] && arr[1][1] === arr[2][0]) {
+            winner.innerHTML = "Player " + arr[0][2] +  " is the winner";
+            displayturn.innerHTML = "";
+            if(arr[0][2] == 'X') { player1 += 1; player1score.innerHTML = player1; }
+            else { player2 += 1; player2score.innerHTML = player2; }
+            removeListeners();
+            clearInterval(countdown);
+            // clearInterval(timerPlayer);
+            clearTimeout(time);
+            document.getElementById("playertimer").innerHTML = "";
+            document.getElementById("timer").innerHTML = "";
+            return;
         }
     }
 }
