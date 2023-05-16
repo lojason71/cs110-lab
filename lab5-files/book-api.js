6eb69b114f848bba1a4f8bb69cf27c4a81e4798b
NathanKong06 <81611522+NathanKong06@users.noreply.github.com>
2023-05-15 18:47:25 -0700

    Files

---
+++ lab5-files/book-api.js
@@ -0,0 +1,39 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors');
+
+const app = express();
+const port = 3000;
+//Where we will keep books
+let books = []
+app.use(cors());
+//Configuring body parser middleware 
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.post('/book',(req,res) => {
+    const book = req.body;
+    //Output the book to the console for debugging
+    console.log(book);
+    books.push(book);
+    res.send('Book is added to the database');
+});
+
+app.get('/books',(req,res) => {
+    res.json(books);
+});
+
+app.post('/book/:isbn',(req,res) => {
+    const isbn = req.params.isbn;
+    const newBook = req.body;
+
+    for (let i = 0; i < books.length; i++) {
+        let book = books[i]
+        if (book.isbn === isbn){
+            books[i] = newBook;
+        }
+    }
+    res.send("Book is edited");
+});
+
+app.listen(port,() => console.log('Hello world app listening on port 3000'));
---
+++ lab5-files/book-list.js
@@ -0,0 +1,72 @@
+    async function loadBooks(){
+        let response = await fetch('http://localhost:3000/books');
+
+        console.log(response.status);
+        console.log(response.statusText);
+
+        if (response.status === 200) {
+            let data = await response.text();
+            console.log(data);
+            const books = JSON.parse(data);
+
+            for (let book of books) {
+                const x = `
+                    <div class ="col-4">
+                        <div class="card">
+                            <div class="card-body">
+                                <h5 class="card-title">${book.title}</h5>
+                                <h6 class="card-subtitle mb-2 text-muted">${book.isbn} </h6>
+
+                                <div>Author:${book.author}</div>
+                                <div>Title:${book.title} </div>
+                                <div>Publisher:${book.publisher} </div>
+                                <div>Number Of Pages: ${book.numpages}</div>
+                                <div>Publish Date:${book.date}</div>
+                                <div>ISBN: ${book.isbn}</div>
+
+                                <hr>
+
+                                <button type="button" class="btn btn-danger">Delete</button>
+                                <button type="button" class="btn btn-danger">
+                                    <data-target="#editBookModal" onClick="setEditModal($book.ibsn})">
+                                    Edit
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                `
+                document.getElementById('books').innerHTML =document.getElementById('books').innerHTML + x;
+            }
+        }
+    }
+
+    loadBooks();
+
+    async function setEditModal(){
+        let response = await fetch('http://localhost:3000/book/${isbn}');
+        console.log(response.status)//200
+        console.log(response.statusText) //Ok
+        if (response.status === 200){
+            let data = await response.text();
+            console.log(data);
+            const book = JSON.parse(data);
+
+            const{
+                title,
+                author,
+                publisher,
+                publish_date,
+                numOfPages,
+            } = book;
+
+            document.getElementById('isbn').value = isbn;
+            document.getElementById('title').value = title;
+            document.getElementById('author').value = author;
+            document.getElementById('publisher').value = publisher;
+            document.getElementById('date').value = publish_date;
+            document.getElementById('numpages').value = numOfPages;
+
+            document.getElementById('editForm').action = "http://localhost:3000/book/${isbn}"
+
+        }
+    }
